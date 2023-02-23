ac11b4c091eac1970bcbf11b15ff7ae49135666f
CaueMB <100325209+CaueMB@users.noreply.github.com>
2023-02-23 20:54:54 -0300

    Update Criando_Tabelas.sql

--- Criando_Tabelas.sql
+++ Criando_Tabelas.sql
@@ -4,16 +4,24 @@ USE Projeto_final_FS_DB;
 GO
 
 --Criando as tabelas de funcionario e departamento
-CREATE TABLE Departamento(
-	Id INT,
-	Nome_Departamento VARCHAR(30)
-);
+CREATE TABLE [dbo].[Departamento](
+	[Id_Dep][INT] IDENTITY(1, 1) NOT NULL,
+	[Nome_Departamento][VARCHAR](50) NOT NULL
+	CONSTRAINT [PK_Departamento] PRIMARY KEY CLUSTERED
+	(
+		[Id_Dep] ASC
+	)WITH(PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)ON [PRIMARY]
+)ON [PRIMARY];
 
-CREATE TABLE Funcionario(
-	Id INT,
-	Nome_Funcionario VARCHAR(45),
-	Nome_Depart VARCHAR(30),
-	Dia_Contratacao DATE
-);
+CREATE TABLE [dbo].[Funcionario](
+	[Id_Fun][INT] IDENTITY(1, 1) NOT NULL,
+	[Nome_Funcionario][VARCHAR](50) NOT NULL,
+	[Nome_Depart][VARCHAR](50) NOT NULL,
+	[Dia_Contratacao][DATE] NULL
+	CONSTRAINT [PK_Funcionario] PRIMARY KEY CLUSTERED
+	(
+		[Id_Fun] ASC
+	)WITH(PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)ON [PRIMARY]
+)ON [PRIMARY];
 
 GO
