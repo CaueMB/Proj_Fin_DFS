af22d74d5d4c03a85ec4fda6d32528b0c322998f
CaueMB <100325209+CaueMB@users.noreply.github.com>
2023-03-24 20:00:03 -0300

    Inserido a opção de Atualizar os registros

--- FrontEnd/src/app/components/atualizar-depart/atualizar-depart.component.ts
+++ FrontEnd/src/app/components/atualizar-depart/atualizar-depart.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//Importação/referência do service
+import { DepartamentoAPIService } from 'src/app/serviceAPI/departamento.api.service';
+//Importação/referência das classes Router - para redirecionamento entre comps e ActivatedRoute - para "cópia" das rotas
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-atualizar-depart',
   templateUrl: './atualizar-depart.component.html',
   styleUrls: ['./atualizar-depart.component.css']
 })
-export class AtualizarDepartComponent {
+export class AtualizarDepartComponent implements OnInit{
 
+  //Definir o titulo comp
+  titleComp: string = 'Atualizar/Alterar dados'
+
+  //Definir uma prop para receber dados
+  DepartamentoAtualizarDado: any = {}
+
+  //Definir os objetos referênciais para as DIs
+  constructor(
+    private departApi: DepartamentoAPIService,
+    private routing: Router,
+    private copiarRota: ActivatedRoute
+  ){}
+
+  //Criar uma propriedade para atuar como a "cópia" da rota usada pelo registro
+  copiandoRota: any = this.copiarRota.snapshot.params['id']
+
+  //Fazer uso do Angular hook para "priorizar" o carregamento de algum conteúdo no componente
+  ngOnInit(): void {
+    //Aqui, é necessário resgatar o registro identificado
+    this.departApi.PegarUmDosDepartamentos(this.copiandoRota).subscribe((DepartDadoAtual: any) => {
+      this.DepartamentoAtualizarDado = DepartDadoAtual
+    })
+  }
+
+  //Criar o método/função que vai acessar o service que chama a API para atualizar/alterar os dados
+  AtualizarDepartamentoRegistro(){
+    //Chamar a DI para acessar o service
+    this.departApi.AtualizarDepartamento(this.DepartamentoAtualizarDado.id, this.DepartamentoAtualizarDado).subscribe(() => {
+      this.routing.navigate(['/pegar-todos-depart'])
+    })
+  }
 }
--- FrontEnd/src/app/components/atualizar-func/atualizar-func.component.ts
+++ FrontEnd/src/app/components/atualizar-func/atualizar-func.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//Importação/referência do service
+import { FuncionarioAPIService } from 'src/app/serviceAPI/funcionario.api.service.sepc';
+//Importação/referência das classes Router - para redirecionamento entre comps e ActivatedRoute - para "cópia" das rotas
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-atualizar-func',
   templateUrl: './atualizar-func.component.html',
   styleUrls: ['./atualizar-func.component.css']
 })
-export class AtualizarFuncComponent {
+export class AtualizarFuncComponent implements OnInit{
+  //Definir o titulo comp
+  titleComp: string = 'Atualizar/Alterar dados'
 
+  //Definir uma prop para receber dados
+  FuncionarioAtualizarDado: any = {}
+
+  //Definir os objetos referênciais para as DIs
+  constructor(
+    private funcApi: FuncionarioAPIService,
+    private routing: Router,
+    private copiarRota: ActivatedRoute
+  ){}
+
+  //Criar uma propriedade para atuar como a "cópia" da rota usada pelo registro
+  copiandoRota: any = this.copiarRota.snapshot.params['id']
+
+  //Fazer uso do Angular hook para "priorizar" o carregamento de algum conteúdo no componente
+  ngOnInit(): void {
+    //Aqui, é necessário resgatar o registro identificado
+    this.funcApi.PegarUmFuncionario(this.copiandoRota).subscribe((FuncDadoAtual: any) => {
+      this.FuncionarioAtualizarDado = FuncDadoAtual
+    })
+  }
+
+  //Criar o método/função que vai acessar o service que chama a API para atualizar/alterar os dados
+  AtualizarFuncionarioRegistro(){
+    //Chamar a DI para acessar o service
+    this.funcApi.AtualizarFuncionario(this.FuncionarioAtualizarDado.id, this.FuncionarioAtualizarDado).subscribe(() => {
+      this.routing.navigate(['/pegar-todos-func'])
+    })
+  }
 }
