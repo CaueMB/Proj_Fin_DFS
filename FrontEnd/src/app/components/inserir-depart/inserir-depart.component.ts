c44e46a48e6a7fbaac371ecdcd1ef504d1277395
CaueMB <100325209+CaueMB@users.noreply.github.com>
2023-03-23 23:18:29 -0300

    Acréscimo de Incluir Registros

--- FrontEnd/src/app/components/inserir-depart/inserir-depart.component.ts
+++ FrontEnd/src/app/components/inserir-depart/inserir-depart.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+//Importação/referência do service no componente
+import { DepartamentoAPIService } from 'src/app/serviceAPI/departamento.api.service';
+//Importação da classe Router para redirecionamento entre componentes
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inserir-depart',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./inserir-depart.component.css']
 })
 export class InserirDepartComponent {
+  //Definir um titulo para o componente
+  titleComp: string = 'Inserir um registro'
 
+  //Criar uma prop - objeto literal - para receber os valores que virão da view
+  @Input() inserirRegistro = {
+    nome_Departamento: '',
+  }
+
+  //Definir a DI com os objetos referenciais no construtor
+  constructor(
+    private departApi: DepartamentoAPIService,
+    private routing: Router
+  ){}
+
+  //Criar um método/função para acessar o service e enviar os dados para a tarefa assincrona de inserção
+  inserirUmDepartamento(){
+    //chamar DI que acessa a API com o método de inserção de dados
+    this.departApi.InserirRegistroDepartamento(this.inserirRegistro).subscribe(() => {
+      this.routing.navigate(['/pegar-todos-depart'])
+    })
+  }
 }
--- FrontEnd/src/app/components/inserir-func/inserir-func.component.ts
+++ FrontEnd/src/app/components/inserir-func/inserir-func.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+//Importação/referência do service no componente
+import { FuncionarioAPIService } from 'src/app/serviceAPI/funcionario.api.service.sepc';
+//Importação da classe Router para redirecionamento entre componentes
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inserir-func',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./inserir-func.component.css']
 })
 export class InserirFuncComponent {
+  //Definir um titulo para o componente
+  titleComp: string = 'Inserir funcionário'
 
+  //Criar uma prop - objeto literal - para receber os valores que virão da view
+  @Input() inserirRegistro = {
+    nome_Funcionario: '',
+    nome_Depart: '',
+    dia_Contratacao: ''
+  }
+
+  //Definir a DI com os objetos referenciais no construtor
+  constructor(
+    private funcApi: FuncionarioAPIService,
+    private routing: Router
+  ){}
+
+  //Criar um método/função para acessar o service e enviar os dados para a tarefa assincrona de inserção
+  inserirUmFuncionario(){
+    //chamar DI que acessa a API com o método de inserção de dados
+    this.funcApi.InserirRegistroFuncionario(this.inserirRegistro).subscribe(() => {
+      this.routing.navigate(['/pegar-todos-func'])
+    })
+  }
 }
